e7abe8a146bc16e279d219f8a5b525c12303e421
Amjith Ramanujam <amjith.r@gmail.com>
2016-11-04 23:18:04 -0700

    Add a template.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var hoverOutTimer = null;
+
+jQuery(document).ready(function($) {
+  'use strict';
+  resize_intro();
+  jQuery(".left section").stop(true, true).fadeIn('fast');
+  jQuery(".right section").stop(true, true).fadeIn('fast');
+});
+
+jQuery(window).resize(function () {
+  'use strict';
+  resize_intro();
+});
+
+function resize_intro(){
+  page_height =  jQuery(window).height();
+    right_height = jQuery('.right section').height() + 40;
+    left_height = jQuery('.left section').height() + 40;
+
+     if(left_height < page_height) {
+
+      jQuery(".left").css('height',page_height+'px');
+      left_content_margin = ((page_height - left_height) / 2) - 30;
+      jQuery(".left section").css('margin-top',left_content_margin+'px');
+
+     }
+
+     if(right_height < page_height) {
+
+      if(jQuery(window).width() > 800) {
+        right_content_margin = (page_height - right_height) / 2;
+        jQuery(".right section").css('margin-top',right_content_margin+'px');
+      }
+
+     }
+}
